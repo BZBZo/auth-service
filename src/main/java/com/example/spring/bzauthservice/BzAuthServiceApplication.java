1b4a1e1ff23b7f92345917edafe2426ff0dd2ad7
Ninky0 <2020113392@dgu.ac.kr>
2024-11-30 22:46:07 +0900

    init : setting

---
+++ src/main/java/com/example/spring/bzauthservice/BzAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring.bzauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BzAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BzAuthServiceApplication.class, args);
+	}
+
+}
