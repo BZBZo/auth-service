fb66f0cc1cd882db194f60de2493dfdb0aba4b9e
khp0916 <khlove0916@naver.com>
2024-12-26 17:33:04 +0900

    feat : 12/26 quit & edit

--- src/main/java/com/example/spring/bzauthservice/controller/SignApiController.java
+++ src/main/java/com/example/spring/bzauthservice/controller/SignApiController.java
@@ -5,6 +5,7 @@ import com.example.spring.bzauthservice.dto.JoinResponseDTO;
 import com.example.spring.bzauthservice.dto.DuplicateResponseDTO;
 import com.example.spring.bzauthservice.dto.SecurityUserDto;
 import com.example.spring.bzauthservice.entity.Member;
+import com.example.spring.bzauthservice.repository.MemberRepository;
 import com.example.spring.bzauthservice.repository.RefreshTokenRepository;
 import com.example.spring.bzauthservice.service.MemberService;
 import com.example.spring.bzauthservice.token.RefreshToken;
@@ -15,12 +16,15 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.view.RedirectView;
 
+import java.util.Map;
 import java.util.Optional;
 
 @Slf4j
@@ -32,6 +36,8 @@ public class SignApiController implements SignApiControllerDocs {
     private final MemberService memberService;
     private final RefreshTokenRepository tokenRepository;
     private final JwtUtil jwtUtil;
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final MemberRepository memberRepository;
     private static final Logger logger = LoggerFactory.getLogger(SignApiController.class);
 
     @PostMapping("/join")
@@ -201,4 +207,43 @@ public class SignApiController implements SignApiControllerDocs {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("유효하지 않은 토큰입니다.");
         }
     }
+
+    @PutMapping("/user/update/{field}")
+    public ResponseEntity<?> updateUserField(
+            @RequestHeader("Authorization") String authorizationHeader,
+            @PathVariable String field,
+            @RequestBody Map<String, String> value) {
+        String token = authorizationHeader.replace("Bearer ", "");
+        Optional<RefreshToken> refreshToken = refreshTokenRepository.findByAccessToken(token);
+        if (refreshToken.isPresent()) {
+            String email = refreshToken.get().getId();
+            String provider = refreshToken.get().getProvider();
+            Optional<Member> member = memberService.findByEmailAndProvider(email, provider);
+            if (member.isPresent()) {
+                Member updatedMember = memberService.updateMemberField(member.get(), field, value.get(field));
+                return ResponseEntity.ok(updatedMember);
+            }
+        }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("유효하지 않은 토큰입니다.");
+    }
+
+//    @PostMapping("/user/update/{field}")
+//    public ResponseEntity<?> updateUserImage(
+//            @RequestHeader("Authorization") String authorizationHeader,
+//            @PathVariable String field,
+//            @RequestPart("file") MultipartFile file) {
+//        String token = authorizationHeader.replace("Bearer ", "");
+//        Optional<RefreshToken> refreshToken = refreshTokenRepository.findByAccessToken(token);
+//        if (refreshToken.isPresent()) {
+//            String email = refreshToken.get().getId();
+//            String provider = refreshToken.get().getProvider();
+//            Optional<Member> member = memberService.findByEmailAndProvider(email, provider);
+//            if (member.isPresent()) {
+//                Member updatedMember = memberService.updateMemberImage(member.get(), field, file);
+//                return ResponseEntity.ok(updatedMember);
+//            }
+//        }
+//        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("유효하지 않은 토큰입니다.");
+//    }
+
 }
